5addf1e32c0a7a37ff15a20aaed2870197dda29b
Luka <luka.basek@gmail.com>
2018-06-18 14:43:58 +0200

    Added csv dataset.

--- dataset.py
+++ dataset.py
@@ -1,39 +1,45 @@
-class DatasetReader:
-    def __init__(self, filename):
+import pandas as pd
+from tqdm import tqdm
+
+
+class Dataset:
+    def __init__(self, filename, output_filename, debug=False):
         self.filename = filename
-        self.IGNORE_WORD = "-X-"
+        self.debug = debug
+        self.output_filename = output_filename
+        self.data_frame = pd.DataFrame(columns=['Sentence #', 'Word', 'POS', 'Chunk', 'Tag'])
 
-    def process_words(self):
+    def create(self):
+        print("-------Creating dataset-------")
+
         f = open(self.filename, "r+")
-        words = []
-        sentences = []
-        for line in f.readlines():
-            if len(line.strip()) > 0:
-                word = self.process_line(line)
-                if word.pos != self.IGNORE_WORD:
-                    words.append(word)
-            else:
-                if len(words) > 0:
-                    sentences.append(words)
-                    words = []
-        if len(words) > 0:
-            sentences.append(words)
-        return sentences
 
-    def process_line(self, line):
+        non_allow_chars = ['"', ',', '-X-']
+
+        iterator = 1
+        for line in tqdm(f.readlines()):
+            line = line.replace(',', '.')
+            row = self.__process_line(line)
+
+            if not (row is None or row[0] in non_allow_chars or row[1] in non_allow_chars):
+                word = row[0]
+                pos = row[1]
+                chunk = row[2]
+                tag = row[3]
+
+                self.data_frame.loc[iterator] = [iterator, word, pos, chunk, tag]
+                iterator += 1
+                if self.debug:
+                    print(
+                        '#{}, \t Word:{}, \t POS:{}, \t Chunk:{}, \t Tag:{}'.format(str(iterator), word, pos, chunk,
+                                                                                    tag))
+
+        self.data_frame.to_csv(self.output_filename + '.csv', encoding='utf-8')
+
+        print("Done! File: " + self.output_filename + ".csv")
+
+    @staticmethod
+    def __process_line(line):
         parts = line.strip().split()
         if len(parts) == 4:
-            return TaggedWord(parts[0], parts[1], parts[2], parts[3])
-        return TaggedWord("", self.IGNORE_WORD, "", "")
-
-
-class TaggedWord:
-    def __init__(self, word, pos, phrase, tag):
-        self.word = word
-        self.pos = pos
-        self.phrase = phrase
-        # TODO: Figure out how to handle boundaries
-        self.tag = tag.split("-")[1] if tag.startswith("I-") or tag.startswith("B-") else tag
-
-    def __str__(self):
-        return "({}, {}, {}, {})".format(self.word, self.pos, self.phrase, self.tag)
+            return parts
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import dataset
+import pandas as pd
 
-print("Hello!")
-
-dataset = dataset.DatasetReader("./dataset/eng.train")
-sentences = dataset.process_words()
-
-print("stop")
+data = pd.read_csv('./dataset/csv/train.csv', encoding='utf-8', delimiter='\t')
+data = data.fillna(method="ffill")
+print(data.tail(50))
+#
+# train_dataset = dataset.Dataset("./dataset/raw/valid.txt", "./dataset/csv/valid", debug=False).create()
